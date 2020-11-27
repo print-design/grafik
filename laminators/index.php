1defc07de214804935c78293783dcd20957f3c95
user <user@Dmitry>
2020-11-27 13:53:34 +0300

    Ламинаторщики1

--- laminators/index.php
+++ laminators/index.php
@@ -4,6 +4,128 @@
         <?php
         include '../include/head.php';
         
+        // Выбор первого ламинаторщика
+        if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['laminator1_id'])) {
+            $laminator1_id = $_POST['laminator1_id'];
+            if($_POST['laminator1_id'] == '') $laminator1_id = "NULL";
+            $sql = '';
+            
+            if(isset($_POST['id'])) {
+                $id = $_POST['id'];
+                $sql = "update laminators set laminator1_id=$laminator1_id where id=$id";
+            }
+            else {
+                $date = $_POST['date'];
+                $shift = $_POST['shift'];
+                $sql = "insert into laminators (date, shift, laminator1_id) values ('$date', '$shift', $laminator1_id)";
+            }
+            
+            $error_message = ExecuteSql($sql);
+        }
+        
+        // Создание нового первого ламинаторщика
+        if($_SERVER['REQUET_METOD'] == 'POST' && isset($_POST['laminator1'])) {
+            $laminator1 = addslashes($_POST['laminator1']);
+            $sql_user = "insert into user (fio, username) values ('$laminator1', CURRENT_TIMESTAMP())";
+            $conn_user = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
+            if($conn_user->query($sql_user) === true) {
+                $laminator1_id = $conn_user->insert_id;
+                
+                $role_id = 4;
+                $sql_role = "insert into user_role (user_id, role_d) values ($laminator1_id, $role_id)";
+                $error_message = ExecuteSql($sql_role);
+                if($error_message = '') {
+                    $sql = '';
+                    
+                    if(isset($_POST['id'])) {
+                        $id = $_POST['id'];
+                        $sql = "update laminators set laminator1_id=$laminator1_id where id=$id";
+                    }
+                    else {
+                        $date = $_POST['date'];
+                        $shift = $_POST['shift'];
+                        $sql = "insert into laminators (date, shift. laminator1_id) values ('$date', '$shift', $laminator1_id)";
+                    }
+                    $error_message = ExecuteSql($sql);
+                }
+            }
+            else {
+                $error_message = $conn_user->error;
+            }
+            
+            $conn_user->close();
+        }
+        
+        // Выбор второго ламинаторщика
+        if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['laminator2_id'])) {
+            $laminator2_id = $_POST['laminator2_id'];
+            if($_POST['laminator2_id'] == '') $laminator2_id = "NULL";
+            $sql = '';
+            
+            if(isset($_POST['id'])) {
+                $id = $_POST['id'];
+                $sql = "update laminators set laminator2_id=$laminator2_id where id=$id";
+            }
+            else {
+                $date = $_POST['date'];
+                $shift = $_POST['shift'];
+                $sql = "insert into laminators (date, shift, laminator2_id) values ('$date', '$shift', $laminator2_id)";
+            }
+            
+            $error_message = ExecuteSql($sql);
+        }
+        
+        // Создание нового второго ламинаторщика
+        if($_SERVER['REQUET_METOD'] == 'POST' && isset($_POST['laminator2'])) {
+            $laminator2 = addslashes($_POST['laminator2']);
+            $sql_user = "insert into user (fio, username) values ('$laminator2', CURRENT_TIMESTAMP())";
+            $conn_user = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
+            if($conn_user->query($sql_user) === true) {
+                $laminator2_id = $conn_user->insert_id;
+                
+                $role_id = 4;
+                $sql_role = "insert into user_role (user_id, role_d) values ($laminator2_id, $role_id)";
+                $error_message = ExecuteSql($sql_role);
+                if($error_message = '') {
+                    $sql = '';
+                    
+                    if(isset($_POST['id'])) {
+                        $id = $_POST['id'];
+                        $sql = "update laminators set laminator2_id=$laminator2_id where id=$id";
+                    }
+                    else {
+                        $date = $_POST['date'];
+                        $shift = $_POST['shift'];
+                        $sql = "insert into laminators (date, shift, laminator2_id) values ('$date', '$shift', $laminator2_id)";
+                    }
+                    $error_message = ExecuteSql($sql);
+                }
+            }
+            else {
+                $error_message = $conn_user->error;
+            }
+            
+            $conn_user->close();
+        }
+        
+        // Заказчик
+        if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['organization'])) {
+            $organization = addslashes($_POST['organization']);
+            $sql = '';
+            
+            if(isset($_POST['id'])) {
+                $id = $_POST['id'];
+                $sql = "update zbs set organization='$organization' where id=$id";
+            }
+            else {
+                $date = $_POST['date'];
+                $shift = $_POST['shift'];
+                $sql = "insert into zbs (date, shift, organization, nn) values ('$date', '$shift', '$organization', $nn)";
+            }
+            
+            $error_message = ExecuteSql($sql);
+        }
+        
         // Получение начальной даты и конечной даты
         include '../include/date_from_date_to.php';
         ?>
@@ -22,7 +144,7 @@
             ?>
             <div class="d-flex justify-content-between mb-2">
                 <div class="p-1">
-                    <h1>Ламинаторы</h1>
+                    <h1>Ламинаторщики</h1>
                 </div>
                 <div class="p-1">
                     <?php
@@ -52,8 +174,8 @@
                         <th></th>
                         <th>Дата</th>
                         <th>Смена</th>
-                        <th>Ламинатор 1</th>
-                        <th>Ламинатор 2</th>
+                        <th>Ламинаторщик 1</th>
+                        <th>Ламинаторщик 2</th>
                         <th>Заказчик</th>
                         <th>Тираж</th>
                         <th>Метраж</th>
