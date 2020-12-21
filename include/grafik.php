6f6865cc6470fad4bd0af8f4d04311aeafa2fc45
user <user@Dmitry>
2020-12-21 14:05:48 +0300

    Изменение размеров

--- include/grafik.php
+++ include/grafik.php
@@ -770,7 +770,7 @@ class Grafik {
             echo "<input type='hidden' id='machine_id' name='machine_id' value='". $this->machineId."' />";
             echo "<table class='in-cell'>";
             echo "<tr>";
-            echo "<td class='$shift'><input type='date' id='date' name='date' /></td>";
+            echo "<td class='$shift'><input type='date' id='date' name='date' style='width:140px;' /></td>";
             echo "<td><select id='shift' name='shift'><option value=''>...</option><option value='day'>День</option><option value='night'>Ночь</option></select></td>";
             echo "<td><button type='submit' id='copy_edition_submit' name='copy_edition_submit' class='btn btn-outline-dark btn-sm' title='Копировать тираж'><span class='font-awesome'>&#xf0c5;</span></button></td>";
             echo "</tr>";
