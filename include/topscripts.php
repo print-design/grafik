c88b72f7de32a5b7ce2c1ea916d6d984510be0df
user <user@Dmitry>
2020-12-21 10:26:11 +0300

    Переделка логина.

--- include/topscripts.php
+++ include/topscripts.php
@@ -119,68 +119,58 @@ $login_username_valid = '';
 $login_password_valid = '';
 
 // Обработка отправки формы логина
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login_submit'])){
-    if($_POST['login_username'] == '') {
+$login_submit = filter_input(INPUT_POST, 'login_submit');
+if($login_submit !== null){
+    $login_username = filter_input(INPUT_POST, 'login_username');
+    if($login_username == '') {
         $login_username_valid = LOGINISINVALID;
         $login_form_valid = false;
     }
     
-    if($_POST['login_password'] == '') {
+    $login_password = filter_input(INPUT_POST, 'login_password');
+    if($login_password == '') {
         $login_password_valid = LOGINISINVALID;
         $login_form_valid = false;
     }
     
     if($login_form_valid) {
-        $login_user_id = '';
-        $login_username = '';
-        $login_fio = '';
-        $login_roles = '';
-
-        $conn = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
-        mysqli_query($conn, 'set names utf8');
-        $sql = "select id, username, fio from user where username='".$_POST['login_username']."' and password=password('".$_POST['login_password']."')";
+        $user_id = '';
+        $username = '';
+        $fio = '';
         
-        if($conn->connect_error) {
-            die('Ошибка соединения: ' . $conn->connect_error);
+        $users_result = (new Grabber("select id, username, fio from user where username='$login_username' and password=password('$login_password') and quit = 0"))->result;
+        
+        foreach ($users_result as $row) {
+            $user_id = $row['id'];
+            setcookie(USER_ID, $user_id, 0, "/");
+            
+            $username = $row['username'];
+            setcookie(USERNAME, $username, 0, "/");
+            
+            $fio = $row['fio'];
+            setcookie(FIO, $fio, 0, "/");
         }
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0 && $row = $result->fetch_assoc()) {
-            $login_user_id = $row['id'];
-            setcookie(USER_ID, $row['id'], 0, "/");
-            
-            $login_username = $row['username'];
-            setcookie(USERNAME, $row['username'], 0, "/");
-            
-            $login_fio = $row['fio'];
-            setcookie(FIO, $row['fio'], 0, "/");
+        
+        if($user_id == '' || $username == '') {
+            $error_message = "Неправильный логин или пароль";
         }
         else {
-            $error_message = "Неправильный логин или пароль.";
-        }
-        
-        if($login_user_id != '') {
-            $role_sql = "select r.name from user_role ur inner join role r on ur.role_id = r.id where ur.user_id = ".$login_user_id;
-            $role_result = $conn->query($role_sql);
-            if($role_result->num_rows > 0) {
-                $roles = array();
-                $role_i = 0;
-                while ($role_row = $role_result->fetch_assoc()) {
-                    $roles[$role_i++] = $role_row['name'];
-                }
-                
-                setcookie(ROLES, serialize($roles), 0, '/');
+            $roles = array();
+            $role_i = 0;
+            $roles_result = (new Grabber("select r.name from user_role ur inner join role r on ur.role_id = r.id where ur.user_id = $user_id"))->result;
+            
+            foreach ($roles_result as $role_row) {
+                $roles[$role_i++] = $role_row['name'];
             }
-        }
-        
-        $conn->close();
-        
-        if($login_username != '') {
+            
+            setcookie(ROLES, serialize($roles), 0, '/');
             header("Refresh:0");
         }
     }
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['logout_submit'])) {
+$logout_submit = filter_input(INPUT_POST, 'logout_submit');
+if($logout_submit !== null) {
     setcookie(USER_ID, '', 0, "/");
     setcookie(USERNAME, '', 0, "/");
     setcookie(FIO, '', 0, "/");
